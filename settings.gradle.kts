3af756c362c589abc13168d5fc61c6d7b3ffcfdd
AnteKoz <Ante.Kozina@adesso.de>
2024-06-11 08:44:57 +0200

    feat: Adding new training module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,4 @@ include("training-romanNumeralsEncoder")
 findProject("training-romanNumeralsEncoder")?.name = "training-romanNumeralsEncoder"
 include("training-disemovelTrolls")
 findProject("training-disemovelTrolls")?.name = "training-disemovelTrolls"
+include("training-predictYourAge")
